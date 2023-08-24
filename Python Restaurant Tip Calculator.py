b3f35e18af5ad569dc04279ebaa0292fe56b1559
Rohit <138593928+RohitDaivat@users.noreply.github.com>
2023-08-24 12:35:20 +0530

    Update Python Restaurant Tip Calculator.py

--- Python Restaurant Tip Calculator.py
+++ Python Restaurant Tip Calculator.py
@@ -1,18 +1,1 @@
-print("Welcome to the python restaurant tip calculator.")
-total_bill = input("what was the total bill? $")
-total_bill = float(total_bill)
 
-percent_tip = input("What Percentage tip would you like to give? 10, 12 or 15? ")
-percent_tip = int(percent_tip)
-
-total_ppl = input("how many people to split the bill? ")
-total_ppl = int(total_ppl)
-
-# calculation wala part
-
-total_percent = (percent_tip / 100) * total_bill
-total_percentbill = total_percent + total_bill
-
-split_bill = total_percentbill / total_ppl
-
-print(f"Each person should pay ${split_bill}")
